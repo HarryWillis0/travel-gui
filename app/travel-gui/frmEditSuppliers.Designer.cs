3d164b45488331b8a6333f4043f5b3b8e6183e98
Harry Willis <harry.willis@edu.sait.ca>
2020-02-15 12:08:07 -0700

    Edit supplier functionality

--- app/travel-gui/frmEditSuppliers.Designer.cs
+++ app/travel-gui/frmEditSuppliers.Designer.cs
@@ -28,12 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Label supplierIdLabel;
+            System.Windows.Forms.Label supNameLabel;
+            this.supplierName = new System.Windows.Forms.TextBox();
+            this.supplierId = new System.Windows.Forms.TextBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.Cancelbtn = new System.Windows.Forms.Button();
+            supplierIdLabel = new System.Windows.Forms.Label();
+            supNameLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // supplierName
+            // 
+            this.supplierName.Location = new System.Drawing.Point(148, 83);
+            this.supplierName.Margin = new System.Windows.Forms.Padding(2);
+            this.supplierName.Name = "supplierName";
+            this.supplierName.Size = new System.Drawing.Size(224, 20);
+            this.supplierName.TabIndex = 29;
+            // 
+            // supplierId
+            // 
+            this.supplierId.Location = new System.Drawing.Point(148, 61);
+            this.supplierId.Margin = new System.Windows.Forms.Padding(2);
+            this.supplierId.Name = "supplierId";
+            this.supplierId.Size = new System.Drawing.Size(92, 20);
+            this.supplierId.TabIndex = 28;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(235, 131);
+            this.btnUpdate.Margin = new System.Windows.Forms.Padding(2);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(66, 25);
+            this.btnUpdate.TabIndex = 27;
+            this.btnUpdate.Text = "Edit";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // Cancelbtn
+            // 
+            this.Cancelbtn.Location = new System.Drawing.Point(148, 131);
+            this.Cancelbtn.Margin = new System.Windows.Forms.Padding(2);
+            this.Cancelbtn.Name = "Cancelbtn";
+            this.Cancelbtn.Size = new System.Drawing.Size(74, 25);
+            this.Cancelbtn.TabIndex = 26;
+            this.Cancelbtn.Text = "Cancel";
+            this.Cancelbtn.UseVisualStyleBackColor = true;
+            this.Cancelbtn.Click += new System.EventHandler(this.Cancelbtn_Click);
+            // 
+            // supplierIdLabel
+            // 
+            supplierIdLabel.AutoSize = true;
+            supplierIdLabel.Location = new System.Drawing.Point(84, 63);
+            supplierIdLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            supplierIdLabel.Name = "supplierIdLabel";
+            supplierIdLabel.Size = new System.Drawing.Size(60, 13);
+            supplierIdLabel.TabIndex = 24;
+            supplierIdLabel.Text = "Supplier Id:";
+            // 
+            // supNameLabel
+            // 
+            supNameLabel.AutoSize = true;
+            supNameLabel.Location = new System.Drawing.Point(84, 86);
+            supNameLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            supNameLabel.Name = "supNameLabel";
+            supNameLabel.Size = new System.Drawing.Size(60, 13);
+            supNameLabel.TabIndex = 25;
+            supNameLabel.Text = "Sup Name:";
+            // 
+            // frmEditSuppliers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmEditSuppliers";
+            this.ClientSize = new System.Drawing.Size(492, 269);
+            this.Controls.Add(this.supplierName);
+            this.Controls.Add(this.supplierId);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.Cancelbtn);
+            this.Controls.Add(supplierIdLabel);
+            this.Controls.Add(supNameLabel);
+            this.Name = "frmEditSuppliers";
+            this.Text = "Edit Supplier";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox supplierName;
+        private System.Windows.Forms.TextBox supplierId;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button Cancelbtn;
     }
 }
--- app/travel-gui/frmEditSuppliers.cs
+++ app/travel-gui/frmEditSuppliers.cs
@@ -11,11 +11,31 @@ using TravelExpertsData;
 
 namespace travel_gui
 {
+    // Form to edit supplier - Raymond
     public partial class frmEditSuppliers : Form
     {
         public frmEditSuppliers(Supplier suppliers)
         {
             InitializeComponent();
+            supplierId.Text = suppliers.SupplierId.ToString();
+            supplierName.Text = suppliers.SupName;
+        }
+
+        private void Cancelbtn_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+        }
+
+        // Update supplier 
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            Supplier supplier = new Supplier()
+            {
+                SupplierId = Int32.Parse(supplierId.Text),
+                SupName = supplierName.Text
+            };
+            SuppliersDB.EditSupplier(supplier);
+            this.DialogResult = DialogResult.OK;
         }
     }
 }
