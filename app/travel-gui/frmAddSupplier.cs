2763b946bb2384e3a1e1ff7062c1aa638e4ec9d3
Harry Willis <harry.willis@edu.sait.ca>
2020-02-15 16:47:01 -0700

    catch primary key can't be duplicated error

--- app/travel-gui/frmAddSupplier.cs
+++ app/travel-gui/frmAddSupplier.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -35,8 +36,24 @@ namespace travel_gui
                     SupplierId = Int32.Parse(supplierId.Text),
                     SupName = supplierName.Text
                 };
-                SuppliersDB.AddSupplier(supplier);
-                this.DialogResult = DialogResult.OK;
+                try
+                {
+                    SuppliersDB.AddSupplier(supplier);
+                    this.DialogResult = DialogResult.OK;
+                }
+                catch (SqlException ex) 
+                {
+                    if (ex.Number == 2627) // supplier id already exists 
+                    {
+                        MessageBox.Show("Supplier ID already exists.", "ID Error");
+                    }
+                } 
+                catch (Exception ex)
+                {
+
+                    MessageBox.Show("Error while adding supplier.\n\n" + ex.Message,
+                        ex.GetType().ToString());
+                }
             }
         }
     }
